a55d00681988d8f5d081804485bad39abdb9177d
Hannah Alverson <alverson98@yahoo.com>
2022-09-13 17:02:34 -0600

    fixed syntax errors, altered employee table to have id start at 1, and added not nulls

--- db/schema.sql
+++ db/schema.sql
@@ -1,46 +1,32 @@
+DROP DATABASE IF EXISTS staff_db;
 CREATE DATABASE staff_db;
 
 USE staff_db;
 
--- 3 tables needed department, role, & employee
-
--- Department --> 
-    -- id: INT PRIMARY KEY
-    -- name: VARCHAR(30) to hold department name
 
 CREATE TABLE department (
-id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-name VARCHAR(30),
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
 );
-
--- role --> 
-    -- id: INT PRIMARY KEY
-    -- title: VARCHAR(30) -- to hold role title
-    -- salary: DECIMAL to hold role salary
-    -- department_id: INT to hold reference to the department role belongs to
 
 CREATE TABLE role (
-id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-title VARCHAR(30),
-salary DECIMAL,
-department_id INT,
-FOREIGN KEY (department_id)
-REFERENCES department(id)
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(8,2) NOT NULL,
+    department_id INT NOT NULL,
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
 );
-
--- employee -->
-    -- id: INT PIMARY KEY
-    -- first_name: VARCHAR(30) to hold employee first name
-    -- last_name: VARCHAR(30) to hold employee last name
-    -- role_id: INT to hold reference to eployee role
-    -- manager_id: INT to hold reference to another employee that is manager of the current employee (null if the employee has no manager)
 
 CREATE TABLE employee (
-id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-first_name VARCHAR(30),
-last_name VARCHAR(30),
-role_id: INT,
-manager_id: INT,
-FOREIGN KEY (role_id)
-REFERENCES roles(id)
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT NULL,
+    FOREIGN KEY (role_id)
+    REFERENCES role(id)
 );
+
+-- to fix issue where employee id starting at 9
+ALTER TABLE employee AUTO_INCREMENT=1;
