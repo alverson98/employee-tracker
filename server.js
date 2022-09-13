5c1e3c9afaa1978870c571a1687d89cafb8393b0
Hannah Alverson <alverson98@yahoo.com>
2022-09-12 18:16:34 -0600

    created outline for querying the database and added comments for to-do's

---
+++ server.js
@@ -0,0 +1,59 @@
+// Dependencies
+const inquirer = require("inquirer");
+const mysql = require("mysql2");
+const consoleTable = require("console.table");
+
+// connecting to server db
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "staff_db",
+  },
+  console.log("Connected to the staff database.")
+);
+
+// User action prompts
+//ask what the user wants to do (view, add)
+// .then() - run function based on user choice
+
+// QUERYING THE DATABASE:
+
+// department table
+//want to see department id & name
+db.query('', function (err, results) {
+    console.log(results);
+})
+
+// role table
+//want to see role id, title, department name, & salary
+db.query('', function (err, results) {
+    console.log(results);
+}) 
+
+// employee table
+//want to see employee id, first name, last name, role title, department name, salary, and manager name
+db.query('', function (err, results) {
+    console.log(results);
+})
+
+
+
+// GIVEN a command-line application that accepts user input
+// WHEN I start the application
+// THEN I am presented with the following options: view all departments, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role
+// WHEN I choose to view all departments
+// THEN I am presented with a formatted table showing department names and department ids
+// WHEN I choose to view all roles
+// THEN I am presented with the job title, role id, the department that role belongs to, and the salary for that role
+// WHEN I choose to view all employees
+// THEN I am presented with a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
+// WHEN I choose to add a department
+// THEN I am prompted to enter the name of the department and that department is added to the database
+// WHEN I choose to add a role
+// THEN I am prompted to enter the name, salary, and department for the role and that role is added to the database
+// WHEN I choose to add an employee
+// THEN I am prompted to enter the employee’s first name, last name, role, and manager, and that employee is added to the database
+// WHEN I choose to update an employee role
+// THEN I am prompted to select an employee to update and their new role and this information is updated in the database
