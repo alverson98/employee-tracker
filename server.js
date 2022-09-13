9da28f8871b6e53d2cc6ec71b62fef1b3e3bab87
Hannah Alverson <alverson98@yahoo.com>
2022-09-12 21:04:15 -0600

    created function to add department and comments to organize the other functions needing to be added

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ const menuSelect = () => {
     });
 };
 
-// QUERYING THE DATABASE:
+// QUERYING THE DATABASE: view requested table(s) & data
 
 // view department table
 const showDepartment = () =>
@@ -89,17 +89,37 @@ const showEmployee = () =>
     }
   );
 
-// GIVEN a command-line application that accepts user input
-// WHEN I start the application
-// THEN I am presented with the following options: view all departments, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role
-// WHEN I choose to view all departments
-// THEN I am presented with a formatted table showing department names and department ids
-// WHEN I choose to view all roles
-// THEN I am presented with the job title, role id, the department that role belongs to, and the salary for that role
-// WHEN I choose to view all employees
-// THEN I am presented with a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
-// WHEN I choose to add a department
-// THEN I am prompted to enter the name of the department and that department is added to the database
+// ADDING TABLE DATA:
+
+// adding department
+const addDepartment = () => {
+  inquirer
+    .createPromptModule([
+      {
+        name: "departmentName",
+        message: "What is the name of the Department you would like to add?",
+        type: "input",
+      },
+    ])
+    .then((response) => {
+      db.query(
+        `INSERT INTO department(name) VALUES ("${response.departmentName}")`
+      );
+      menuSelect();
+    });
+};
+
+//adding role
+const addRole = () => 
+
+//adding employee
+
+//UPDATING TABLE DATA:
+
+//updating employee role
+
+
+
 // WHEN I choose to add a role
 // THEN I am prompted to enter the name, salary, and department for the role and that role is added to the database
 // WHEN I choose to add an employee
