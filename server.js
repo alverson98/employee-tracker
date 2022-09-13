db3f6001c8fb5db1f6295f38a8d598ce016f760e
Hannah Alverson <alverson98@yahoo.com>
2022-09-13 17:44:11 -0600

    fixed syntax issue when selecting data from employee table

--- server.js
+++ server.js
@@ -82,7 +82,7 @@ const showRole = () =>
 // view employee table
 const showEmployee = () =>
   db.query(
-    "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, employee.salary, manager.first_name AS manager FROM employee LEFT JOIN role  ON employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id LEFT JOIN employee manager ON manager.id = employee.manager_id",
+    "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary AS salary, manager.first_name AS manager FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id LEFT JOIN employee manager ON manager.id = employee.manager_id",
     (err, results) => {
       console.table(results);
       menuSelect();
